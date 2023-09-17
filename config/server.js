45dd9a6b37f215184310d37f8fa450fbbad30258
dongdong <dongdong@192.168.nate.com>
2023-09-18 01:45:39 +0900

    test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e327e296e4528240e6fa358579c73e18'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4b3bacdb-dc49-42e7-9aab-f4a0e4f3f586'
+};
